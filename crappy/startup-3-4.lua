5a2f130cf311bc2f2fcec5b33af7369ca57ab3ed
Andrew Phillips <theasp@gmail.com>
2014-06-16 11:54:39 -0300

    Fix switching tags

--- crappy/startup-3-4.lua
+++ crappy/startup-3-4.lua
@@ -58,9 +58,9 @@ function startup.widget.taglist(s)
    local mytaglist = {}
    mytaglist.buttons = awful.util.table.join(
       awful.button({ }, 1, awful.tag.viewonly),
-      awful.button({ modkey }, 1, awful.client.movetotag),
+      awful.button({ crappy.ezconfig.modkey }, 1, awful.client.movetotag),
       awful.button({ }, 3, awful.tag.viewtoggle),
-      awful.button({ modkey }, 3, awful.client.toggletag),
+      awful.button({ crappy.ezconfig.modkey }, 3, awful.client.toggletag),
       awful.button({ }, 4, awful.tag.viewnext),
       awful.button({ }, 5, awful.tag.viewprev)
    )
