415c3b8f2b21f6976d38fc9b2c8aa66b990661da
Andrew Phillips <theasp@gmail.com>
2014-09-14 15:35:17 -0300

    UI tweak

--- awesome-config/startup/fallback.lua
+++ awesome-config/startup/fallback.lua
@@ -7,21 +7,40 @@ local fallback = {}
 
 function fallback.buildUi(settings)
    local settingsJson = json:encode_pretty(settings)
-
-   print("Settings:\n" .. settingsJson)
-
    local buffer = Gtk.TextBuffer {}
-
    local iter = buffer:get_iter_at_offset(0)
    buffer:insert(iter, settingsJson, -1)
-
    local textview = Gtk.TextView {
       buffer = buffer,
    }
-
    textview:override_font(Pango.font_description_from_string('Monospace'))
 
-   return textview
+   local applyButton = Gtk.Button {
+      use_stock = true,
+      label = Gtk.STOCK_APPLY,
+   }
+
+   return Gtk.ScrolledWindow {
+      shadow_type = 'ETCHED_IN',
+      expand = true,
+      Gtk.Box {
+         orientation = 'VERTICAL',
+         spacing = 6,
+         margin = 6,
+         expand = true,
+         Gtk.ScrolledWindow {
+            shadow_type = 'ETCHED_IN',
+            expand = true,
+            textview
+         },
+         Gtk.Box {
+            orientation = 'HORIZONTAL',
+            spacing = 6,
+            halign = 'END',
+            applyButton
+         }
+      }
+   }
 end
 
 return fallback
