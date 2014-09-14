1b63d79c39afab32077d9dd76cf702c4166f042f
Andrew Phillips <theasp@gmail.com>
2014-09-13 21:38:47 -0300

    Moved config file handling to despicable

---
+++ despicable/init.lua
@@ -0,0 +1,63 @@
+local despicable = {}
+
+despicable.configver = 0.1
+
+despicable.json = require('despicable.JSON')
+despicable.default = require('despicable.default')
+
+function despicable.new()
+   return despicable.default.config()
+end
+
+function despicable.load(file)
+   print("Loading despicable file " .. file .. "...")
+
+   -- TODO: Error handling
+   local f = assert(io.open(file, "r"), "Unable to open file: " .. file)
+   local configJson = f:read("*all")
+   f:close()
+
+   local config = despicable.json:decode(configJson)
+
+   if config.configver ~= despicable.configver then
+      print("The configuration in " .. file .. " is not a supported, using default")
+      config = despicable.default.config()
+   end
+
+   return config
+end
+
+function despicable.show(config)
+   print("JSON:\n" .. despicable.json:encode_pretty(config))
+end
+
+function despicable.save(file, config)
+   print("Saving despicable file " .. file .. "...")
+
+   -- TODO: Error handling
+   local f = assert(io.open(file, "w"), "Unable to open file: " .. file)
+   f:write(despicable.json:encode_pretty(config))
+   f:close()
+end
+
+function despicable.json:onDecodeError(message, text, location, etc)
+   if text then
+      if location then
+         message = string.format("Error reading JSON at char %d: %s", location, message)
+      else
+         message = string.format("Error reading JSON: %s", message)
+      end
+   end
+
+   if etc ~= nil then
+      message = message .. " (" .. OBJDEF:encode(etc) .. ")"
+   end
+
+   if self.assert then
+      self.assert(false, message)
+   else
+      assert(false, message)
+   end
+end
+
+return despicable
