d0b9fac4e8ffd97529102f3430b2df033578b82a
Andrew Phillips <theasp@gmail.com>
2014-10-27 10:52:20 -0300

    Change scope of variables, update UI better on changes

--- lib/crappy/gui/init.lua
+++ lib/crappy/gui/init.lua
@@ -16,23 +16,26 @@ gui.authors = {
    "Andrew Phillips",
 }
 gui.plugins = require('crappy.gui.plugins')
-gui.pluginTabs = {}
-gui.file = nil
 
 function gui.on_startup(app)
    pluginManager.loadAllPlugins()
-   gui.config = configManager.new()
 end
 
 function gui.on_shutdown(app)
 end
 
 function gui.on_activate(app)
+   local file = nil
+   local pluginTabs = {}
+   local pluginsUi = nil
+   local config = configManager.new()
+   local mainNotebook = nil
+
    -- Function to add a plugin tab
    local function addPluginTab(plugin)
       local pluginLog = lgi.log.domain('gui/' .. plugin.id)
       local label = Gtk.Label { label = plugin.name }
-      local settings = gui.config.plugins[plugin.id].settings
+      local settings = config.plugins[plugin.id].settings
 
       -- Use the plugin's buildUi function to populate the tab,
       -- otherwise use the fallback.
@@ -46,29 +49,25 @@ function gui.on_activate(app)
       -- Don't add a tab if the plugin's buildUi function returned nil
       if ui then
          ui:show_all()
-         gui.mainNotebook:append_page(ui, label)
-         table.insert(gui.pluginTabs, ui)
+         mainNotebook:append_page(ui, label)
       end
+
+      return ui
    end
 
    -- Function to reset the GUI based on the current config
    local function updateUi()
-      if (gui.pluginsUi) then
-         gui.pluginsUi:destroy()
+      configManager.makeFullConfig(config)
+
+      if not pluginsUi then
+         pluginsUi = gui.plugins.buildUi(window, config, updateUi)
+         local pluginsUiLabel = Gtk.Label { label = "Plugins"}
+
+         pluginsUi:show_all()
+         mainNotebook:append_page(pluginsUi, pluginsUiLabel)
       end
 
-      configManager.makeFullConfig(gui.config)
-
-      gui.pluginsUi = gui.plugins.buildUi(window, gui.config)
-      gui.pluginsUiLabel = Gtk.Label { label = "Plugins"}
-      gui.pluginsUi:show_all()
-      gui.mainNotebook:append_page(gui.pluginsUi, gui.pluginsUiLabel)
-
-      for i, pluginTab in pairs(gui.pluginTabs) do
-         pluginTab:destroy()
-      end
-
-      local enabledPlugins = configManager.getEnabledPlugins(gui.config)
+      local enabledPlugins = configManager.getEnabledPlugins(config)
       table.sort(enabledPlugins,
                  function(a,b)
                     if a.name < b.name then
@@ -77,9 +76,34 @@ function gui.on_activate(app)
                  end
       )
 
-      for i, plugin in ipairs(enabledPlugins) do
-         addPluginTab(plugin)
+      -- Add all new enabled pluginTabs
+      for pluginId, pluginInfo in pairs(config.plugins) do
+         if not pluginTabs[pluginId] and enabledPlugins[pluginId] then
+            pluginTabs[pluginId] = addPluginTab(pluginManager.plugins[pluginId])
+         end
       end
+
+      -- Remove existing tabs that aren't enabled
+      for pluginId, pluginTab in pairs(pluginTabs) do
+         if not enabledPlugins[pluginId] then
+            pluginTab:destroy()
+            pluginTabs[pluginId] = nil
+         end
+      end
+   end
+
+   local function resetUi()
+      for pluginId, pluginTab in pairs(pluginTabs) do
+         pluginTab:destroy()
+      end
+      pluginTabs = {}
+
+      if pluginsUi then
+         pluginsUi:destroy()
+         pluginsUi = nil
+      end
+
+      updateUi()
    end
 
    local function quit()
@@ -94,21 +118,21 @@ function gui.on_activate(app)
    local function newFile()
       log.message('New file')
 
-      gui.config = configManager.new()
-      updateUi()
+      config = configManager.new()
+      resetUi()
    end
 
    local function loadFile()
-      log.message("Loading file " .. gui.file)
+      log.message("Loading file " .. file)
 
-      gui.config = configManager.load(gui.file)
-      updateUi()
+      config = configManager.load(file)
+      resetUi()
    end
 
    local function saveFile()
       log.message('Save file')
 
-      configManager.save(gui.file, gui.config)
+      configManager.save(file, config)
    end
 
    local function about()
@@ -159,28 +183,28 @@ function gui.on_activate(app)
    ui:insert_action_group(actions, 0)
 
    local ok, err = ui:add_ui_from_string(
-[[
-<ui>
-  <menubar name='MenuBar'>
-    <menu action='FileMenu'>
-      <menuitem action='New'/>
-      <menuitem action='Open'/>
-      <menuitem action='Save'/>
-      <menuitem action='SaveAs'/>
-      <separator/>
-      <menuitem action='Quit'/>
-    </menu>
-    <menu action='HelpMenu'>
-      <menuitem action='About'/>
-    </menu>
-  </menubar>
-  <toolbar  name='ToolBar'>
-    <toolitem action='Open'/>
-    <toolitem action='Save'/>
-    <toolitem action='Quit'/>
-  </toolbar>
-</ui>
-]], -1)
+      [[
+               <ui>
+               <menubar name='MenuBar'>
+               <menu action='FileMenu'>
+               <menuitem action='New'/>
+               <menuitem action='Open'/>
+               <menuitem action='Save'/>
+               <menuitem action='SaveAs'/>
+               <separator/>
+               <menuitem action='Quit'/>
+            </menu>
+               <menu action='HelpMenu'>
+               <menuitem action='About'/>
+            </menu>
+               </menubar>
+               <toolbar  name='ToolBar'>
+               <toolitem action='Open'/>
+               <toolitem action='Save'/>
+               <toolitem action='Quit'/>
+            </toolbar>
+               </ui>
+      ]], -1)
 
    if not ok then
       log.message('building menus failed: %s', err)
@@ -195,7 +219,7 @@ function gui.on_activate(app)
       on_destroy = quit
    }
 
-   gui.mainNotebook = Gtk.Notebook {
+   mainNotebook = Gtk.Notebook {
       scrollable = true,
    }
 
@@ -203,7 +227,7 @@ function gui.on_activate(app)
                  orientation = 'VERTICAL',
                  ui:get_widget('/MenuBar'),
                  ui:get_widget('/ToolBar'),
-                 gui.mainNotebook
+                 mainNotebook
    })
 
    window:add_accel_group(ui:get_accel_group())
@@ -212,7 +236,7 @@ function gui.on_activate(app)
    window:show_all()
 
    HOME=os.getenv('HOME')
-   gui.file = HOME .. "/.config/awesome/crappy.json"
+   file = HOME .. "/.config/awesome/crappy.json"
    loadFile()
 
    return window
