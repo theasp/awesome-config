97ee73ebf8f6f77066ef446d585f2aca54544ead
Andrew Phillips <theasp@gmail.com>
2014-10-28 12:02:28 -0300

    Only merge a value from t2 if t1 is nil

--- lib/crappy/misc.lua
+++ lib/crappy/misc.lua
@@ -20,7 +20,9 @@ function misc.mergeTable(t1, t2)
       if (type(v) == "table") and (type(t1[k] or false) == "table") then
          misc.mergeTable(t1[k], t2[k])
       else
-         t1[k] = v
+         if t1[k] == nil then
+            t1[k] = v
+         end
       end
    end
    return t1
