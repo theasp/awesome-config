1bd9039615a825a00276103f8f5ec1c7eb2b5c61
Andrew Phillips <theasp@gmail.com>
2014-10-28 12:25:18 -0300

    Replace buildUi with options table

--- lib/crappy/crappy-plugins/crappy.startup.settings.lua
+++ lib/crappy/crappy-plugins/crappy.startup.settings.lua
@@ -8,12 +8,35 @@ local plugin = {
    id = 'crappy.startup.settings',
    requires = {},
    provides = {"crappy.shared.settings.titlebar", "crappy.shared.settings.sloppyfocus", "crappy.shared.settings.terminal", "crappy.shared.settings.editor", "crappy.shared.settings"},
+   options = {
+      {
+         name = 'terminal',
+         label = "Te_rminal Emulator:",
+         type = "string"
+      },
+      {
+         name = 'editor',
+         label = "_Editor:",
+         type = "string"
+      },
+      {
+         name = 'titlebar',
+         label = "Show _Titlebar",
+         type = "boolean"
+      },
+      {
+         name = 'sloppyfocus',
+         label = "_Sloppy Focus",
+         type = "boolean"
+      }
+   },
    defaults = {
       terminal = "x-terminal-emulator",
       titlebar = true,
       sloppyfocus = true
    }
 }
+
 local log = lgi.log.domain(plugin.id)
 
 function plugin.settingsDefault(settings)
@@ -31,101 +54,6 @@ function plugin.startup(awesomever, settings)
    local beautiful = misc.use("beautiful")
 
    shared.settings = settings
-end
-
-function plugin.buildUi(window, settings)
-   local Gtk = lgi.require('Gtk')
-
-   local titlebarCheckButton = Gtk.CheckButton {
-      label = 'Show _Titlebar',
-      use_underline = true,
-   }
-   titlebarCheckButton:set_active(settings.titlebar)
-
-   function titlebarCheckButton:on_toggled()
-      settings.titlebar = titlebarCheckButton:get_active()
-   end
-
-   local sloppyfocusCheckButton = Gtk.CheckButton {
-      label = '_Sloppy Focus',
-      use_underline = true,
-   }
-
-   sloppyfocusCheckButton:set_active(settings.sloppyfocus)
-
-   function sloppyfocusCheckButton:on_toggled()
-      settings.sloppyfocus = sloppyfocusCheckButton:get_active()
-   end
-
-   local terminalEntry = Gtk.Entry {
-      hexpand = true,
-   }
-
-   terminalEntry:set_text(settings.terminal)
-   function terminalEntry:on_changed()
-      settings.terminal = terminalEntry:get_text()
-   end
-
-   local editorEntry = Gtk.Entry {
-      hexpand = true,
-   }
-
-   editorEntry:set_text(settings.editor)
-
-   function editorEntry:on_changed()
-      settings.editor = editorEntry:get_text()
-   end
-
-   local row = -1;
-   local function nextRow()
-      row = row + 1
-      return row
-   end
-
-   return Gtk.Grid {
-      row_spacing = 6,
-      column_spacing = 6,
-      margin = 6,
-      expand = true,
-
-      {
-         left_attach = 0, top_attach = nextRow(),
-         titlebarCheckButton
-      },
-
-      {
-         left_attach = 0, top_attach = nextRow(),
-         sloppyfocusCheckButton
-      },
-
-      {
-         left_attach = 0, top_attach = nextRow(),
-         Gtk.Label {
-            label = 'Te_rminal Emulator:',
-            halign = 'END',
-            use_underline = true,
-            mnemonic_widget = terminalEntry
-         },
-      },
-      {
-         left_attach = 1, top_attach = row,
-         terminalEntry
-      },
-
-      {
-         left_attach = 0, top_attach = nextRow(),
-         Gtk.Label {
-            label = '_Editor:',
-            halign = 'END',
-            use_underline = true,
-            mnemonic_widget = editorEntry
-         },
-      },
-      {
-         left_attach = 1, top_attach = row,
-         editorEntry
-      },
-   }
 end
 
 return plugin
--- lib/crappy/crappy-plugins/crappy.startup.theme.lua
+++ lib/crappy/crappy-plugins/crappy.startup.theme.lua
@@ -6,6 +6,18 @@ local plugin = {
    description = 'Set the theme used by beautiful',
    id = 'crappy.startup.theme',
    provides = {},
+   options = {
+      {
+         name = 'file',
+         label = "_Theme File:",
+         type = "string"
+      },
+      {
+         name = 'font',
+         label = "Font _Override:",
+         type = "string"
+      },
+   },
    defaults = {
       file = "/usr/share/awesome/themes/default/theme.lua",
       font = ''
@@ -23,77 +35,6 @@ function plugin.startup(awesomever, settings)
       awesome.font = settings.font
       beautiful.get().font = settings.font
    end
-end
-
-function plugin.buildUi(window, settings)
-   local Gtk = lgi.require('Gtk')
-
-   local fileEntry = Gtk.Entry {
-      text = settings.file,
-      expand = true
-   }
-
-   function fileEntry:on_changed()
-      settings.file = fileEntry:get_text()
-   end
-
-   local fontEntry = Gtk.Entry {
-      text = settings.font,
-      expand = true
-   }
-
-   function fontEntry:on_changed()
-      settings.font = fontEntry:get_text()
-   end
-
-
-   local row = -1;
-   local function nextRow()
-      row = row + 1
-      return row
-   end
-
-   return Gtk.Box {
-      orientation = 'VERTICAL',
-      spacing = 6,
-      expand = true,
-      valign = 'START',
-
-      Gtk.Grid {
-         row_spacing = 6,
-         column_spacing = 6,
-         margin = 6,
-         expand = true,
-
-         {
-            left_attach = 0, top_attach = nextRow(),
-            Gtk.Label {
-               label = '_Theme File:',
-               halign = 'END',
-               use_underline = true,
-               mnemonic_widget = fileEntry
-            },
-         },
-         {
-            left_attach = 1, top_attach = row,
-            fileEntry
-         },
-         {
-            left_attach = 0, top_attach = nextRow(),
-            Gtk.Label {
-               label = '_Font Override:',
-               halign = 'END',
-               use_underline = true,
-               mnemonic_widget = fontEntry
-            },
-         },
-         {
-            left_attach = 1, top_attach = row,
-            fontEntry
-         }
-
-      }
-   }
 end
 
 return plugin
