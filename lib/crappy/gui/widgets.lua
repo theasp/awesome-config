50a97eeb6e8683b8b14786d83ef457e6f33d1803
Andrew Phillips <theasp@gmail.com>
2014-12-06 11:29:07 -0400

    Move the JSON editor to widgets

--- lib/crappy/gui/widgets.lua
+++ lib/crappy/gui/widgets.lua
@@ -3,6 +3,8 @@ local functionManager = require('crappy.functionManager')
 local lgi = require 'lgi'
 local Gtk = lgi.require('Gtk')
 local GObject = lgi.require('GObject')
+local Pango = lgi.require('Pango')
+local json = require('crappy.JSON')
 
 local widgets = {}
 
@@ -326,5 +328,134 @@ function widgets.functionComboBox(valid, current)
    return comboBox
 end
 
+function widgets.jsonEditor(window, settings, applyCallback)
+   local settingsJson = json:encode_pretty(settings)
+   local buffer = Gtk.TextBuffer {}
+   buffer.text = settingsJson
+
+   local textview = Gtk.TextView {
+      buffer = buffer,
+   }
+   textview:override_font(Pango.font_description_from_string('Monospace'))
+
+   local undoButton = Gtk.Button {
+      use_stock = true,
+      label = Gtk.STOCK_UNDO,
+   }
+
+   local applyButton = Gtk.Button {
+      use_stock = true,
+      label = Gtk.STOCK_APPLY,
+   }
+
+   local function disableButtons()
+      undoButton:set_sensitive(false)
+      applyButton:set_sensitive(false)
+   end
+
+   local function enableButtons()
+      undoButton:set_sensitive(true)
+      applyButton:set_sensitive(true)
+   end
+
+   function applyButton:on_clicked()
+      local ok = true
+      local errstr
+
+      function json:assert(state, message)
+      end
+
+      function json:onDecodeError(message, text, location, etc)
+         if text then
+            if location then
+               message = string.format("%s at char %d", message, location)
+            else
+               message = string.format("%s", message)
+            end
+         end
+
+         if etc ~= nil then
+            message = message .. " (" .. OBJDEF:encode(etc) .. ")"
+         end
+
+         errstr = message
+         ok = false
+
+         if self.assert then
+            self.assert(false, message)
+         else
+            assert(false, message)
+         end
+      end
+
+      newSettings = json:decode(buffer.text)
+
+      if ok and type(newSettings) == 'table' then
+         -- Clear the settings table, we want to keep the same
+         -- reference
+         for k in pairs(settings) do
+            settings[k] = nil
+         end
+
+         for k, v in pairs(newSettings) do
+            settings[k] = v
+         end
+
+         settingsJson = json:encode_pretty(settings)
+         buffer.text = settingsJson
+         disableButtons()
+         if applyCallback then
+            applyCallback()
+         end
+      else
+         if not errstr then
+            errstr = "Unkown error decoding JSON"
+         end
+
+         log.message("Error: %s", errstr)
+         if errstr then
+            local dialog = Gtk.MessageDialog {
+               transient_for = window,
+               destroy_with_parent = true,
+               message_type = 'ERROR',
+               buttons = 'CLOSE',
+               text = "Error:\n" .. errstr,
+               on_response = Gtk.Widget.destroy
+            }
+            dialog:show_all()
+         end
+      end
+   end
+
+   function buffer:on_changed(modifed)
+      enableButtons()
+   end
+
+   function undoButton:on_clicked()
+      buffer.text = settingsJson
+      disableButtons()
+   end
+
+   disableButtons()
+
+   return Gtk.Box {
+      orientation = 'VERTICAL',
+      spacing = 6,
+      margin = 6,
+      expand = true,
+      Gtk.ScrolledWindow {
+         shadow_type = 'ETCHED_IN',
+         expand = true,
+         textview
+      },
+      Gtk.Box {
+         orientation = 'HORIZONTAL',
+         spacing = 6,
+         halign = 'END',
+         undoButton,
+         applyButton
+      }
+   }
+end
 
 return widgets
